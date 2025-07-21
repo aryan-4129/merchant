30f9bca7ab4129941b8b6e05aa9424e9e188d91b
aryan-4129 <150704582+aryan-4129@users.noreply.github.com>
2025-07-21 15:47:23 +0530

    Update MerchantNApplicationTests.java

--- src/test/java/com/enroll/merchantN/MerchantNApplicationTests.java
+++ src/test/java/com/enroll/merchantN/MerchantNApplicationTests.java
@@ -1,19 +1,19 @@
-//package com.enroll.merchantN;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest
-//@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
-//class MerchantNApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//
-//}
+package com.enroll.merchantN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+class MerchantNApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+}
